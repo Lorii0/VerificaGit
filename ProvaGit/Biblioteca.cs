87294daba37f9fb21e28c11d0efc6ad050fadd49
Lorii0 <146861747+Lorii0@users.noreply.github.com>
2024-10-21 09:09:11 +0200

    Release 0.1.1: aggiunti vari commenti

--- ProvaGit/Biblioteca.cs
+++ ProvaGit/Biblioteca.cs
@@ -14,8 +14,8 @@ namespace VerificaGit
         
 
         string indirizzo;
-        string apertura;
-        string chiusura;
+        string apertura;    //orario 
+        string chiusura;    //orario
 
         public string Nome { get { return nome; }set { nome = value; } }
         public string Indirizzo { get { return indirizzo; } set { indirizzo = value; } }
@@ -37,16 +37,16 @@ namespace VerificaGit
             libri.Add(libro);
         }
 
-        public Libro RicercaLibro(string titolo)
+        public Libro RicercaLibro(string titolo)    //ritorna libro singolo
         {
             for (int i = 0; i < libri.Count; i++)
             {
-                if (libri[i].Titolo == titolo) return libri[i];
+                if (libri[i].Titolo == titolo) return libri[i]; 
             }
             throw new Exception();
         }
 
-        public List<Libro> RicercaLibriAutore(string autore)
+        public List<Libro> RicercaLibriAutore(string autore)    //ritorna lista di libri
         {
             List<Libro> l = new List<Libro>();
             for (int i = 0; i < libri.Count; i++)
