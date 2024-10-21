55272e96e730b089e381d354bae003ea39137b3b
Lorii0 <146861747+Lorii0@users.noreply.github.com>
2024-10-21 08:37:34 +0200

    first commit

---
+++ ProvaGit/Biblioteca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaGit
+{
+    internal class Biblioteca
+    {
+        Biblioteca(string nome, string indirizzo, DateTime apertura, DateTime chiusura) { }
+    }
+}
